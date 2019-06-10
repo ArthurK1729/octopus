d7d5d368d8e74416d3ce0a9b8fdd52b8e78662d0
ArthurK1729 <a.krochin@hotmail.com>
2019-06-10 23:49:50 +0100

    Add adjlist ingestion; add voteToHalt mechanism; add bigger test graph

--- octopus.go
+++ octopus.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"io"
 	"log"
 	"math"
+	"os"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
@@ -25,25 +30,31 @@ type Vertex struct {
 	state           VertexState
 	active          bool
 	outNeighbourIds []uint32
+	voteToHalt      bool
 }
 
 func (v *Vertex) compute(message Message) {
 	if v.state.shortestPathSize > message.candidateShortestPath {
 		v.state.shortestPathSize = message.candidateShortestPath
+		// If a vertex has updated its state, it will want to broadcast it
+		v.voteToHalt = false
 	}
 }
 
 func (v *Vertex) broadcast(outboxChannel chan Envelope) {
+	log.Println("Broadcasting for", v.vertexID)
+
 	for _, nbdID := range v.outNeighbourIds {
-		log.Println("Broadcasting for", v.vertexID)
 		newState := v.state.shortestPathSize + 1
 		outboxChannel <- Envelope{destinationVertexID: nbdID, message: Message{candidateShortestPath: newState}}
+		// As soon as the vertex has broadcasted its new state, it will want to stop
+		v.voteToHalt = true
 	}
 
 }
 
 func inboxWorker(vertexStore map[uint32]*Vertex, inboxChannel chan Envelope) {
-	log.Println("Worker spawned")
+	log.Println("Inbox worker spawned")
 	defer wg.Done()
 	select {
 	case envelope := <-inboxChannel:
@@ -52,6 +63,7 @@ func inboxWorker(vertexStore map[uint32]*Vertex, inboxChannel chan Envelope) {
 		v.compute(envelope.message)
 		log.Println("State of", envelope.destinationVertexID, "now is", v.state)
 	case <-time.After(1 * time.Second):
+		log.Println("WORKER TIMED OUT")
 		return
 	}
 }
@@ -69,7 +81,7 @@ func inboxWorker(vertexStore map[uint32]*Vertex, inboxChannel chan Envelope) {
 func outboxWorker(vertexStore map[uint32]*Vertex, outboxChannel chan Envelope, inboxChannel chan Envelope) {
 	log.Println("Executing outboxWorker")
 	for _, v := range vertexStore {
-		if v.state.shortestPathSize != math.MaxUint32 {
+		if v.state.shortestPathSize != math.MaxUint32 && v.voteToHalt == false {
 			v.broadcast(outboxChannel)
 		}
 	}
@@ -79,6 +91,7 @@ func outboxWorker(vertexStore map[uint32]*Vertex, outboxChannel chan Envelope, i
 		log.Println(envelope, "sent to inboxChannel")
 		inboxChannel <- envelope
 	case <-time.After(2 * time.Second):
+		log.Println("OUTBOX WORKER TIMED OUT")
 		return
 	}
 }
@@ -89,8 +102,77 @@ func displayFinalResults(vertexStore map[uint32]*Vertex) {
 	}
 }
 
-func ingestGraph(vertexStore map[uint32]*Vertex, path string) {
+// func readAdjlist(vertexStore map[uint32]*Vertex, path string) {
+// 	file, err := os.Open(path)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer file.Close()
 
+// 	scanner := bufio.NewScanner(file)
+// 	for scanner.Scan() {
+// 		fmt.Println(scanner.Text())
+// 	}
+
+// 	if err := scanner.Err(); err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
+
+// Reads in an adjlist and populates vertexStore
+func readAdjlist(vertexStore map[uint32]*Vertex, path string) (err error) {
+	log.Println("Reading", path)
+
+	file, err := os.Open(path)
+	defer file.Close()
+
+	if err != nil {
+		return err
+	}
+
+	reader := bufio.NewReader(file)
+
+	var line string
+	for {
+		line, err = reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		nodes := strings.Split(line, " ")
+		log.Println("Loading node tuple: ", nodes)
+		originNode, _ := strconv.Atoi(strings.TrimSpace(nodes[0]))
+		targetNodes := make([]uint32, len(nodes[1:]))
+
+		for index, node := range nodes[1:] {
+			n, _ := strconv.Atoi(strings.TrimSpace(node))
+			targetNodes[index] = uint32(n)
+		}
+
+		vertexStore[uint32(originNode)] = &Vertex{
+			vertexID:        uint32(originNode),
+			state:           VertexState{shortestPathSize: math.MaxUint32},
+			outNeighbourIds: targetNodes,
+			voteToHalt:      false,
+		}
+
+	}
+
+	if err != io.EOF {
+		log.Printf("Failed to read graph: %v\n", err)
+	}
+
+	return
+}
+
+func countUnvisited(vertexStore map[uint32]*Vertex) {
+	count := 0
+	for _, v := range vertexStore {
+		if v.state.shortestPathSize == math.MaxUint32 {
+			count++
+		}
+	}
+	log.Println("Number of unvisited:", count)
 }
 
 // TODO:
@@ -99,31 +181,33 @@ func ingestGraph(vertexStore map[uint32]*Vertex, path string) {
 // Figure out the unexported names thing
 // Fix broadcasting problem (only should be done for active nodes)
 // Fix issue where node 3 only gets the guaranteed correct value after 5 steps instead of 3
+// Intelligent partitioning scheme. Don't load the whole graph into memory, rather
+// read lines one by one and ship them to other nodes once buffer limit is reached.
+// voteToHalt might not be threadsafe
+// Verify results with single source shortest path in NetworkX
 var wg sync.WaitGroup
 
 func main() {
 	log.Println("Initializing")
 	var vertexStore = make(map[uint32]*Vertex)
-	var inboxChannel = make(chan Envelope, 100)
-	var outboxChannel = make(chan Envelope, 100)
+	var inboxChannel = make(chan Envelope, 100000)
+	var outboxChannel = make(chan Envelope, 100000)
 
-	log.Println("Loading vertices")
-	vertexStore[1] = &Vertex{vertexID: 1, state: VertexState{shortestPathSize: math.MaxUint32}, outNeighbourIds: []uint32{2}}
-	vertexStore[2] = &Vertex{vertexID: 2, state: VertexState{shortestPathSize: math.MaxUint32}, outNeighbourIds: []uint32{3, 4}}
-	vertexStore[3] = &Vertex{vertexID: 3, state: VertexState{shortestPathSize: math.MaxUint32}, outNeighbourIds: []uint32{}}
-	vertexStore[4] = &Vertex{vertexID: 4, state: VertexState{shortestPathSize: math.MaxUint32}, outNeighbourIds: []uint32{3, 5}}
-	vertexStore[5] = &Vertex{vertexID: 5, state: VertexState{shortestPathSize: math.MaxUint32}, outNeighbourIds: []uint32{}}
+	log.Println("Ingesting vertices")
+	readAdjlist(vertexStore, "facebook_social_graph.adjlist")
+	// readAdjlist(vertexStore, "graph.adjlist")
 
-	log.Println("Seed sent")
-	inboxChannel <- Envelope{destinationVertexID: 1, message: Message{candidateShortestPath: 0}}
+	log.Println("Sending seed")
+	inboxChannel <- Envelope{destinationVertexID: 0, message: Message{candidateShortestPath: 0}}
 
-	for i := 0; i < 20; i++ {
+	for i := 0; i < 4*len(vertexStore); i++ {
 		wg.Add(1)
 		go inboxWorker(vertexStore, inboxChannel)
 		wg.Wait()
 		outboxWorker(vertexStore, outboxChannel, inboxChannel)
+		log.Println("ROUND OVER")
 	}
 
 	displayFinalResults(vertexStore)
-
+	countUnvisited(vertexStore)
 }
