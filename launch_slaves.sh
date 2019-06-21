94d7cb423e98c8b0423b1bf49650d30b67ff80d6
Artur Krochin <arturkrochin@arturs-mbp.mynet>
2019-06-21 16:23:41 +0100

    Do minor cleanup

---
+++ launch_slaves.sh
@@ -0,0 +1,5 @@
+nohup ./octopus --mode=slave --concurrencyLevel=4 --slavePort=50052 &
+nohup ./octopus --mode=slave --concurrencyLevel=4 --slavePort=50053 &
+nohup ./octopus --mode=slave --concurrencyLevel=4 --slavePort=50054 &
+nohup ./octopus --mode=slave --concurrencyLevel=4 --slavePort=50055 &
+nohup ./octopus --mode=slave --concurrencyLevel=4 --slavePort=50056 &
--- rpc.go
+++
@@ -1,5 +0,0 @@
-package main
-
-// Ship vertices to host and port
-// Ship messages to host and port
-// Get VertexSlaveRegistry
